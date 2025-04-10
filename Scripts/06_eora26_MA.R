bb05e6d12518afcae4f9216e8c426a289e903cb2
xolaniss-dev <xolaniss@gmail.com>
2025-04-10 21:00:32 +0200

    adjusted code for monthly data

--- Scripts/06_eora26_MA.R
+++ Scripts/06_eora26_MA.R
@@ -3,6 +3,7 @@
 # Preliminaries -----------------------------------------------------------
 # core
 library(tidyverse)
+library(tidyfast)
 library(readr)
 library(readxl)
 library(here)
@@ -16,6 +17,7 @@ library(pins)
 library(timetk)
 library(uniqtag)
 library(quantmod)
+library(data.table)
 
 # graphs
 library(PNWColors)
@@ -39,6 +41,8 @@ library(furrr)
 library(parallel)
 library(tictoc)
 
+mem.maxVSize(100000)
+
 # Functions ---------------------------------------------------------------
 source(here("Functions", "fx_plot.R"))
 
@@ -48,40 +52,94 @@ path_list <-
   years |>
   map(
     ~ glue(
-      "/Users/xolanisibande/Papers/climate_sereno/Data/Eora26/moving_averages/MA_{.}.txt"
+      "~/Papers/climate_sereno/Data/Eora26/moving_averages/MA_{.}.txt"
     )
   )
 
-n_cores <- detectCores() - 1 # leave 2 cores for OS
+n_cores <- detectCores() - 1 # leave 1 core for OS
 plan(multisession, workers = n_cores) # setting multisession plan
 
 tic()
-eora_ma_tbl <-
+eora_ma_yearly_tbl <-
   path_list |>
-  future_map(
-    ~ read_delim(
-      .x,
-      col_names = TRUE,
-      show_col_types = FALSE
-    )
-  ) |>
+  future_map(~ read_delim(.x, col_names = TRUE, show_col_types = FALSE)) |>
   set_names(years) |>
   bind_rows(.id = "year") |>
-  janitor::clean_names() |>
-  pivot_longer(cols = -c(year, country, industry),
-             names_to = "sector",
-             values_to = "ma") |>
-  mutate(sector = str_replace_all(sector, "household_goods","householdgoods" )) |>
-  separate(sector, sep = "_", into = c("source_country", "sector")) |>
-  mutate(source_country  = str_to_upper(source_country))
-
+  # janitor::clean_names() |>
+  mutate(year = as.Date(year, format = "%Y")) |>
+  mutate(year = year  %-time% "3months") |>
+  mutate(year = year  %-time% "9days")
 toc()
 plan(sequential)
 
 
+# Expanding to monthly frequency ------------------------------------------
+expanded_dates_tbl <- rep(seq(
+  from = as.Date("1990-01-01"),
+  to = as.Date("2017-12-01"),
+  by = "month"
+), 189) |>
+  as_tibble() # expanded dates for monthly
+
+country_names_vec <- eora_ma_yearly_tbl |> distinct(country)
+country_names_tbl <- tibble("country" = rep(country_names_vec, times = 336)) |>
+  unnest(country) |>
+  arrange(country) |>
+  tibble("date" = expanded_dates_tbl$value) |>
+  relocate(country, .after = date) # creating monthly date and name combination
+
+
+## Expanding agrifood ------------------------------------------------------
+eora_ma_monthly_agri_food_tbl <-
+  country_names_tbl |>
+  left_join(
+    eora_ma_yearly_tbl |>
+      filter(industry == "agrifood"),
+    by = join_by(date == year, country == country)
+  ) |>
+  fill(c(3:1893), .direction = "down") # monthly for agrifood
+
+
+## Expanding downstream ----------------------------------------------------
+eora_ma_monthly_downstream_tbl <-
+  country_names_tbl |>
+  left_join(
+    eora_ma_yearly_tbl |>
+      filter(industry == "downstream"),
+    by = join_by(date == year, country == country)
+  ) |>
+  fill(c(3:1893), .direction = "down") # monthly for downstream
+
+
+## Combining back to full tbl ----------------------------------------------
+eora_ma_monthly_tbl <-
+  eora_ma_monthly_agri_food_tbl |>
+  bind_rows(eora_ma_monthly_downstream_tbl) |>
+  arrange(country) |>
+  rename(row_country = country) |>
+  rename(row_industry = industry)
+
+
+## Pivoting longer using dt -------------------------------------------------------
+tic()
+eora_ma_monthly_long_tbl <-
+  as.data.table(eora_ma_monthly_tbl) |>
+  dt_pivot_longer(
+    cols = -c(date, row_country, row_industry),
+    names_to = "sector",
+    values_to = "ma"
+  ) |>
+  dt_separate(sector,
+              sep = ".",
+              into = c("column_country", "column_industry")
+              ) |>
+  as_tibble() |>
+  mutate(column_industry  = str_to_lower(column_industry))
+toc()
+
 # Export ---------------------------------------------------------------
 artifacts_combined_eoro26 <- list (
-  eora_ma_tbl = eora_ma_tbl
+  eora_ma_monthly_long_tbl = eora_ma_monthly_long_tbl
 )
 
 write_rds(artifacts_combined_eoro26,
--- Scripts/07_combined_data.R
+++ Scripts/07_combined_data.R
@@ -44,62 +44,66 @@ mem.maxVSize(100000)
 source(here("Functions", "fx_plot.R"))
 
 # Import and clean -------------------------------------------------------------
+## Eora26 data ---------------------------------------------------------------
 eora_ma_tbl <- read_rds(here("Outputs", "artifacts_combined_eoro26.rds")) |>
-  pluck(1) |>
-  mutate(year = as.numeric(year)) |>
-  relocate(source_country, .after = country) # repeat the yearly figure monthly
+  pluck(1)
 
+## CPI data ---------------------------------------------------------------
 cpi_tbl <- read_rds(here("Outputs", "artifacts_cpi.rds")) |>
   pluck(1) |>
   filter(date > "1990-01-01") |>
-  group_by(category, country_abs) |>
-  summarise_by_time(.date_var = date,
-                    .by = "year",
-                    cpi = mean(cpi, na.rm = FALSE)) |>
-  rename(year = date) |>
-  ungroup() |>
-  relocate(year, .before = category) |>
-  mutate(year = as.numeric(year(year))) |>
+  dplyr::select(-country) |>
   rename(country = country_abs) |>
   mutate(category = str_to_lower(category)) |>
-  mutate(category = str_replace_all(category, "hh goods", "household_goods"))
+  mutate(category = str_replace_all(category, "hh goods", "household_goods")) |>
+  mutate(category = str_replace_all(category, "food & bev", "agrifood")) |>
+  filter(category != "other") |>
+  filter(category != "alcbev") |>
+  filter(category != "recreation")
 
-# take recreation, alcbev, other
-# change food and bev to agrifood
-# ignore other headline
+cpi_industry_tbl <-
+  cpi_tbl |>
+  filter(category != "headline")
 
+cpi_headline_tbl <-
+  cpi_tbl |>
+  filter(category == "headline")
 
+## Climate data ---------------------------------------------------------------
 precipitation_tbl <-
-  read_rds(here("Outputs", "Precipitation", "yearly_precip.rds")) |>
-  relocate(year, .before = country)
+  read_rds(here("Outputs", "Precipitation", "monthly_precip.rds")) |>
+  relocate(year, .before = country) |>
+  dplyr::select(-year, -month) |>
+  filter(date >= "1990-01-01"  & date <= "2017-12-01") |>
+  relocate(country, .after = date)
 
-temp_tbl <- read_rds(here("Outputs", "Temperature", "yearly_temp.rds")) |>
-  relocate(year, .before = country)
+temp_tbl <-
+  read_rds(here("Outputs", "Temperature", "monthly_temp.rds")) |>
+  relocate(year, .before = country) |>
+  dplyr::select(-year, -month) |>
+  filter(date >= "1990-01-01" & date <= "2017-12-01") |>
+  relocate(country, .after = date)
 
 
 # Combining data ---------------------------------------------------------------
 combined_data_tbl <-
   eora_ma_tbl |>
-  mutate(sector = str_replace_all(sector, "householdgoods", "household_goods")) |>
-  left_join(precipitation_tbl, by = c("year", "country")) |>
-  left_join(temp_tbl, by = c("year", "country"))
-
-# left_join(cpi_tbl |>
-#             by = c("year", "country"))
+  left_join(precipitation_tbl, by = join_by(date == date,
+                                            row_country == country)) |>
+  left_join(temp_tbl, by = join_by(date == date,
+                                   row_country == country)) |>
+  left_join(cpi_industry_tbl, by = join_by(date == date,
+                                          column_country == country,
+                                          column_industry == category
+                                          ))
 
 # EDA ---------------------------------------------------------------
-
 combined_data_tbl |> skim()
-# Graphing ---------------------------------------------------------------
 
 
 # Export ---------------------------------------------------------------
 artifacts_combined_data <- list (
-  combined_data_tbl = combined_data_tbl,
-  eora_ma_tbl = eora_ma_tbl,
-  cpi_tbl = cpi_tbl,
-  precipitation_tbl = precipitation_tbl,
-  temp_tbl = temp_tbl
+  combined_data_tbl = combined_data_tbl
 )
 
 write_rds(artifacts_combined_data, file = here("Outputs", "artifacts_combined_data.rds"))
