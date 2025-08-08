74a69a39ac18fa097bc80319809ccea9011a7494
Xolani Sibande <xolaniss@gmail.com>
2025-08-08 12:58:31 -0400

    draft season, income_clas, inflation regs

---
+++ Scripts/16_income_classification_reg.R
@@ -0,0 +1,67 @@
+# Description
+# income classification analysis - Xolani Sibande August 2025
+# Preliminaries -----------------------------------------------------------
+# core
+library(tidyverse)
+library(readr)
+library(readxl)
+library(here)
+library(lubridate)
+library(xts)
+library(broom)
+library(glue)
+library(scales)
+library(kableExtra)
+library(pins)
+library(timetk)
+library(uniqtag)
+library(quantmod)
+
+# graphs
+library(PNWColors)
+library(patchwork)
+
+# eda
+library(psych)
+library(DataExplorer)
+library(skimr)
+
+# econometrics
+library(tseries)
+library(strucchange)
+library(vars)
+library(urca)
+library(mFilter)
+library(car)
+
+# Parallel processing
+library(furrr)
+library(parallel)
+library(tictoc)
+
+# Functions ---------------------------------------------------------------
+source(here("Functions", "fx_plot.R"))
+
+# Import -------------------------------------------------------------
+
+
+# Cleaning -----------------------------------------------------------------
+
+
+# Transformations --------------------------------------------------------
+
+
+# EDA ---------------------------------------------------------------
+
+
+# Graphing ---------------------------------------------------------------
+
+
+# Export ---------------------------------------------------------------
+artifacts_ <- list (
+
+)
+
+write_rds(artifacts_, file = here("Outputs", "artifacts_.rds"))
+
+
